451477c0f675985f12050b3a790d3099a25a45ab
Lee Nugent <lee.nugent@gousto.co.uk>
2021-12-01 10:19:05 +0000

    Team file

---
+++ team_members.py
@@ -0,0 +1,14 @@
+## Obtain your ID by viewing this JSON version of the leaderboard: https://adventofcode.com/2021/leaderboard/private/view/956865.json
+
+TEAM_MEMBERS = {
+    "Carrots": [
+        "956865",  # Lee Nugent
+        "979941",  # Joe Wood
+        "1548925",  # Pedro
+    ],
+    "Pakchois": [
+        "1520356",  # Dan Paddon
+        "1593139",  # James Davies
+    ]
+
+}
