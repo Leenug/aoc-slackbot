7ef055f3dc8ff8e7afa2bd62f8e81bfaa960553c
James Hallam <james.hallam93@gmail.com>
2021-12-01 12:43:15 +0000

    Added team members for Potatoes

--- team_members.py
+++ team_members.py
@@ -1,4 +1,4 @@
-## Obtain your ID by viewing this JSON version of the leaderboard: https://adventofcode.com/2021/leaderboard/private/view/956865.json
+# Obtain your ID by viewing this JSON version of the leaderboard: https://adventofcode.com/2021/leaderboard/private/view/956865.json
 
 TEAM_MEMBERS = {
     "Carrots": [
@@ -11,12 +11,18 @@ TEAM_MEMBERS = {
         "1593139",  # James Davies
     ],
     "Sprouts": [
-        "1520324", # Matt Bell
-        "1522520", # Mike Wyer
+        "1520324",  # Matt Bell
+        "1522520",  # Mike Wyer
         "960018",  # Alex Kelly
-        "1306614", # James Carson
+        "1306614",  # James Carson
     ],
-    "Bento" :  [
-        "635495", # Jamie Coombs
+    "Bento":  [
+        "635495",  # Jamie Coombs
+    ],
+    "Potatoes": [
+        "1143182",  # James Hallam
+        "966635",   # Shane Dowling
+        "1610353",  # Cheuk Ho Chan
+        "1306614",  # James Carson
     ]
 }
