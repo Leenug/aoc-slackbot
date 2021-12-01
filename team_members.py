82a6e17a370be0acab543056547660d9ba51d9fb
Lee Nugent <lee.nugent@gousto.co.uk>
2021-12-01 13:39:39 +0000

    More teams

--- team_members.py
+++ team_members.py
@@ -11,23 +11,32 @@ TEAM_MEMBERS = {
         "1593139",  # James Davies
     ],
     "Sprouts": [
-        "1520324", # Matt Bell
-        "1522520", # Mike Wyer
+        "1520324",  # Matt Bell
+        "1522520",  # Mike Wyer
         "960018",  # Alex Kelly
-        "1306614", # James Carson
+        "1306614",  # James Carson
     ],
     'Kimchi': [
         "959686",  # Sasho Marinov
         "960687",  # Do Young Kim
     ],
-    "Bento" :  [
-        "635495", # Jamie Coombs
+    "Bento": [
+        "635495",  # Jamie Coombs
     ],
-
     "Pumpkins": [
-        "960687", # Nathan
-        "1526226", # Ken
-        "1520343", # Kenny
-        "1520317", # Arjun
+        "960687",  # Nathan
+        "1526226",  # Ken
+        "1520343",  # Kenny
+        "1520317",  # Arjun
     ],
+    "Potatoes": [
+        "1143182",  # James Hallam
+        "966635",  # Shane Dowling
+        "1610353",  # Cheuk Ho Chan
+        "1306614",  # James Carson
+    ],
+    "Kales": [
+        "979912",  # Rianne McCartney
+        "1992",  # James Monger
+    ]
 }
